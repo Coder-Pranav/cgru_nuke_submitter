5d5d55ea9931607169e06178db34aca596473c4c
pranav pradeep <prnvprdp@gmail.com>
2020-08-20 11:48:59 +0530

    a working submitter for nuke

--- nukeCGRUSubmitter.py
+++ nukeCGRUSubmitter.py
@@ -1,7 +1,9 @@
-from PySide2.QtWidgets import *
-import nuke
+import os
 import sys
-import  os
+
+import nuke
+from PySide2.QtWidgets import *
+
 cgruPath = r'E:\Download\compressed_file\cgru.2.3.1.windows\cgru.2.3.1'
 sys.path.append(r'{}\afanasy\python'.format(cgruPath))
 sys.path.append(r'{}\lib\python'.format(cgruPath))
@@ -28,68 +30,64 @@ class Panel(QWidget):
         self.master_layout = QVBoxLayout()
         self.hori_lay_buttons = QHBoxLayout()
 
-        grid = QGridLayout()
+        form_lay = QFormLayout()
 
         self.button1 = QPushButton('ok')
         self.button2 = QPushButton('cancel')
-        self.job_paused = QCheckBox('Submit_Paused')
-        self.job_paused.setStyleSheet("color: blue")
+
+        self.job_paused = QCheckBox()
+        self.job_paused.setStyleSheet("color: Orange")
 
         self.pool = QComboBox()
         self.pool.addItems(pools_list)
         self.pool.setCurrentIndex(1)
 
-        self.label_for_pool = QLabel('Pool')
-
-        self.label_for_submitin = QLabel('Submit_In')
         self.submit_in = QComboBox()
         self.submit_in.addItems(submit_in_list)
 
-        self.label_for_framepertask = QLabel('Frame_Per_Task')
         self.frames_per_task = QComboBox()
         self.frames_per_task.addItems(frame_per_task_list)
+        self.frames_per_task.setCurrentIndex(1)
 
-        for node in self.all_nodes():
-            nf = self.get_node_info(node)
-            self.select_writenodes(nf[0],nf[1],nf[2])
+        self.loadWrite() ### For nuke to find all Write nodes
 
-        grid.addWidget(self.job_paused, 0, 0)
-        grid.addWidget(self.label_for_pool, 1, 0)
-        grid.addWidget(self.pool, 1, 1)
-        grid.addWidget(self.label_for_framepertask, 2, 0)
-        grid.addWidget(self.frames_per_task, 2, 1)
-        grid.addWidget(self.label_for_submitin, 3, 0)
-        grid.addWidget(self.submit_in, 3, 1)
-        grid.setColumnStretch(1, 1)
+        form_lay.addRow('Submit Paused', self.job_paused)
+        form_lay.addRow('Pool', self.pool)
+        form_lay.addRow('Frame Per Task', self.frames_per_task)
+        form_lay.addRow('Submit In', self.submit_in)
 
         # setting ok and cancel
         self.hori_lay_buttons.addWidget(self.button1)
         self.hori_lay_buttons.addWidget(self.button2)
 
-        self.master_layout.addLayout(grid)
+        self.master_layout.addLayout(form_lay)
         self.master_layout.addLayout(self.hori_lay_buttons)
 
         self.setLayout(self.master_layout)
-        self.setWindowTitle('CGRU_RENDER')
+        self.setWindowTitle('CGRU RENDERER')
         self.setMinimumWidth(300)
-        self.setMinimumHeight(350)
 
         self.button2.clicked.connect(self.cancelButton)
         self.button1.clicked.connect(self.submitOk)
 
-    def select_writenodes(self, write_name,frameFirst, lastFrame):
-        frames = '{}:{}'.format(frameFirst,lastFrame)
+
+    def loadWrite(self):
+        self.write_nodes = []
+        for node in self.all_nodes():
+            nf = self.get_node_info(node)
+            self.select_writenodes(nf[0], nf[1], nf[2])
+
+
+    def select_writenodes(self, write_name, frameFirst, lastFrame):
+        frames = '{}:{}'.format(frameFirst, lastFrame)
         check_box = QCheckBox(write_name)
 
         line_edit_frames = QLineEdit()
         line_edit_frames.setText(frames)
+        line_edit_frames.setStyleSheet("color: orange")
 
-        label_edit = QLabel('Frame_Range')
-        label_edit.setStyleSheet("color: orange")
-
-        lay_h = QHBoxLayout()
-        lay_h.addWidget(label_edit)
-        lay_h.addWidget(line_edit_frames)
+        lay_h = QFormLayout()
+        lay_h.addRow('Frame Range', line_edit_frames)
 
         layout_vertical_write = QVBoxLayout()
         layout_vertical_write.addWidget(check_box)
@@ -97,19 +95,18 @@ class Panel(QWidget):
 
         self.master_layout.addLayout(layout_vertical_write)
 
-
-
     def all_nodes(self):
         nodes = nuke.allNodes()
         for node in nodes:
             if node.Class() == 'Write' and node not in self.write_nodes:
-                self.write_nodes.append(node)
-        # self.write_nodes.sort()
+                if not node['disable'].value():
+                    self.write_nodes.append(node)
         return self.write_nodes
 
     def get_node_info(self, writeNode):
         root_framerange = nuke.root()
         self.write_name = writeNode['name'].value()
+        self.seqName = writeNode['file'].value().replace('%04d', '@####@')
 
         if writeNode['use_limit'].value():
             self.write_firstframe = int(writeNode['first'].value())
@@ -118,45 +115,59 @@ class Panel(QWidget):
             self.write_firstframe = int(root_framerange['first_frame'].value())
             self.write_lastframe = int(root_framerange['last_frame'].value())
 
-        return (self.write_name, self.write_firstframe, self.write_lastframe)
-
+        return (self.write_name, self.write_firstframe, self.write_lastframe,self.seqName)
 
     def nukeRootinfos(self):
         self.root = nuke.root()['name'].value()
         self.wn = os.path.basename(self.root)
-        self.jobname= self.wn.split('.')
+        self.jobname = self.wn.split('.')
         self.jobname = self.jobname[0]
         self.workDir = os.path.dirname(self.root)
 
-        return(self.jobname, self.workDir, self.wn)
+        return (self.jobname, self.workDir, self.wn)
 
+    def setJobs(self, writename, framefirst, framelast, framepertask, seqname):
 
-    def setJobs(self,writename,framefirst,framelast,framepertask):
-        job = af.Job(self.nukeRootinfos()[0])
+        """send jobs from nuke to cgru"""
+
+        job_name = '{}_{}'.format(writename, self.nukeRootinfos()[0])
+        job = af.Job(job_name)
         job.setMaxRunningTasks(15)
         block = af.Block('Nuke_Render', 'nuke')
         block.setWorkingDirectory(self.nukeRootinfos()[1])
         block.setCommand('nuke -i -X {} -x {} @#@,@#@'.format(writename, self.nukeRootinfos()[2]))
-        block.setFiles(['jpg/img.@####@.jpg'])
+        block.setFiles([seqname])
         block.setNumeric(framefirst, framelast, framepertask)
         job.blocks.append(block)
+        if self.job_paused.isChecked():
+            job.offline()
         job.send()
 
     def cancelButton(self):
+        """ Cancel Button """
+
         self.close()
 
     def submitOk(self):
+        """ OK Button/Submit """
+
         nuke.scriptSave("")
-        for widget in self.children():
+        write_info = []
+        for widget in self.master_layout.children():
             if isinstance(widget, QVBoxLayout):
-                print widget
-        # for chkbox in self.findChildren(QCheckBox):
-        #     if chkbox.isChecked() and chkbox.text() != 'Submit_Paused':
-        #         nf = self.get_node_info(nuke.toNode(chkbox.text()))
-        #         self.setJobs(nf[0],nf[1],nf[2],int(self.frames_per_task.currentText()))
-        # self.close()
+                writeName_vb = widget.itemAt(0).widget()
+                frameRange_vb = widget.children()
+                frameRange_vb = frameRange_vb[0].itemAt(1).widget()
+                widget_pair = (writeName_vb, frameRange_vb)
+                write_info.append(widget_pair)
 
-
+        for pair_widget in write_info:
+            if pair_widget[0].isChecked():
+                splitter = pair_widget[1].text().split(':')
+                frameF = int(splitter[0])
+                frameL = int(splitter[1])
+                self.setJobs(pair_widget[0].text(), frameF, frameL, int(self.frames_per_task.currentText()), self.get_node_info(nuke.toNode(pair_widget[0].text()))[3])
+                self.close()
 
 
 def run():
@@ -165,4 +176,5 @@ def run():
     run.panel.show()
     # sys.exit(app.exec_())
 
-run()
+
+# run()
